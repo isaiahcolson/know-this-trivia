b4cb729c29c94bc0d9559e84c1e7bed4db9c42e2
colsonisaiah <colson.isaiah@gmail.com>
2020-05-12 10:47:07 -0400

    Basic question styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
-console.log('Test');
+$('.quiz-container h3').html('Who is this?');
+
+$('.quiz-container img').attr('src', 'images/test.png')
+// $('.quiz-container img').attr('src', 'images/texture3.png')
+
+
+$('label[for=optionA]').html('A. Morty');
+$('label[for=optionB]').html('B. Mr. Poopybutthole');
+$('label[for=optionC]').html('C. Pickle Rick');
+$('label[for=optionD]').html('D. Scary Terry');
