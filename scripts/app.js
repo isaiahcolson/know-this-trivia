94d907636005d3c30c04e48c45b212b7edc4b9d1
colsonisaiah <colson.isaiah@gmail.com>
2020-05-12 16:31:32 -0400

    Step 4 - function to check if answer given is correct

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,49 @@
-$('.quiz-container h3').html('Who is this?');
+// $('.quiz-container h3').html('Who is this?');
 
-$('.quiz-container img').attr('src', 'images/test.png')
-// $('.quiz-container img').attr('src', 'images/texture3.png')
+// $('.quiz-container img').attr('src', 'images/test.png')
 
+// $('label[for=optionA]').html('A. Martha Stewart');
+// $('label[for=optionB]').html('B. Mr. Poopybutthole');
+// $('label[for=optionC]').html('C. Pickle Rick');
+// $('label[for=optionD]').html('D. Scary Terry');
 
-let $optionA = $('label[for=optionA]').html('A. Martha Stewart');
-$('label[for=optionB]').html('B. Mr. Poopybutthole');
-$('label[for=optionC]').html('C. Pickle Rick');
-$('label[for=optionD]').html('D. Scary Terry');
+const question1 = {
+    question: 'Who is this?',
+    image: 'images/test.png',
+    optionA: 'A. Martha Stewart',
+    optionB: 'B. Mr. Poopybutthole',
+    optionC: 'C. Pickle Rick',
+    optionD: 'D. Scary Terry',
+    correct: 'C. Pickle Rick'
+};
 
-/* $('input[name=optionA]').on('click', function() {
-    console.log($('input:checked').val());
-}); */
+const displayQuestions = (questionArray) => {
+    $('.quiz-container h3').html(questionArray.question);
+    $('.quiz-container img').attr('src', questionArray.image);
+    $('label[for=optionA]').html(questionArray.optionA);
+    $('label[for=optionB]').html(questionArray.optionB);
+    $('label[for=optionC]').html(questionArray.optionC);
+    $('label[for=optionD]').html(questionArray.optionD);
+}
 
-/* $('input[name=answers]').click(function() {
-    console.log($('input').val());
-}); */
+const checkValue = (array) => {
+    const $answer = $('input[name=answers]:checked').siblings('label').html();
+    const correct = array.correct;
+    if (correct === $answer) {
+        console.log('Correct');
+    } else {
+        console.log('Incorrect');
+    }
+    // console.log(`Correct answer is ${correct}`);
+}
+
+displayQuestions(question1);
+
+// console.log(question1);
 
 $('button').click(function() {
-    console.log('Clicked');
-    const $answer = $('input[name=answers]:checked').siblings('label').html();
-    console.log($answer);
+    // console.log('Clicked');
+    // const $answer = $('input[name=answers]:checked').siblings('label').html();
+    // console.log($answer);
+    checkValue(question1);
 });
