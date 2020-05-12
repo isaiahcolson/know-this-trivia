2b0e03ef8fc206bf78b29b739a26838a465064ad
colsonisaiah <colson.isaiah@gmail.com>
2020-05-12 15:09:08 -0400

    Log input value of users selected response

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,21 @@ $('.quiz-container img').attr('src', 'images/test.png')
 // $('.quiz-container img').attr('src', 'images/texture3.png')
 
 
-$('label[for=optionA]').html('A. Morty');
+let $optionA = $('label[for=optionA]').html('A. Martha Stewart');
 $('label[for=optionB]').html('B. Mr. Poopybutthole');
 $('label[for=optionC]').html('C. Pickle Rick');
 $('label[for=optionD]').html('D. Scary Terry');
+
+/* $('input[name=optionA]').on('click', function() {
+    console.log($('input:checked').val());
+}); */
+
+/* $('input[name=answers]').click(function() {
+    console.log($('input').val());
+}); */
+
+$('button').click(function() {
+    console.log('Clicked');
+    const $answer = $('input[name=answers]:checked').siblings('label').html();
+    console.log($answer);
+});
